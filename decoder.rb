41a88d7490d13684204f343c82bc519f4b9e4b7f
prius2055 <prius2055@gmail.com>
2023-06-14 15:11:43 +0100

    fix linters

--- decoder.rb
+++ decoder.rb
@@ -1,49 +1,45 @@
-
 def decode_char(char)
-    lexicon = {
-    ".-" => "A",
-    "-..." => "B",
-    "-.-." => "C",
-    "-.." => "D",
-    "." => "E",
-    "..-." => "F",
-    "--." => "G",
-    "...." => "H",
-    ".." => "I",
-    ".---" => "J",
-    "-.-" => "K",
-    ".-.." => "L",
-    "--" => "M",
-    "-." => "N",
-    "---" => "O",
-    ".--." => "P",
-    "--.-" => "Q",
-    ".-." => "R",
-    "..." => "S",
-    "-" => "T",
-    "..-" => "U",
-    "...-" => "V",
-    ".--" => "W",
-    "-..-" => "X",
-    "-.--" => "Y",
-    "--.." => "Z"
+  lexicon = {
+    '.-' => 'A',
+    '-...' => 'B',
+    '-.-.' => 'C',
+    '-..' => 'D',
+    '.' => 'E',
+    '..-.' => 'F',
+    '--.' => 'G',
+    '....' => 'H',
+    '..' => 'I',
+    '.---' => 'J',
+    '-.-' => 'K',
+    '.-..' => 'L',
+    '--' => 'M',
+    '-.' => 'N',
+    '---' => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.' => 'R',
+    '...' => 'S',
+    '-' => 'T',
+    '..-' => 'U',
+    '...-' => 'V',
+    '.--' => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'Z'
   }
-    lexicon.each do |key,value|
-    if key == char
-        return value
-    end
-end
+  lexicon.each do |key, value|
+    return value if key == char
+  end
 end
 
-def decode_word (chars)
-mapped_char = chars.split.map {|char| decode_char(char)}
-mapped_char.join
+def decode_word(chars)
+  mapped_char = chars.split.map { |char| decode_char(char) }
+  mapped_char.join
 end
 
-def decode_message (words)
-mapped_message = words.split("  ").map {|char| decode_word(char)}
-mapped_message.join(" ")
+def decode_message(words)
+  mapped_message = words.split('  ').map { |char| decode_word(char) }
+  mapped_message.join(' ')
 end
 
-puts decode_message(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
-
+puts decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
